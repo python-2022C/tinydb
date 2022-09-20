306cd490a1dba8cf036959177bd8f11bcf7ba04d
ZARIFMAC <uzbzarif@gmail.com>
2022-09-20 11:15:24 +0500

    default table name

--- get_id.py
+++ get_id.py
@@ -1,11 +1,15 @@
 from tinydb import TinyDB
 from tinydb.database import Document
+#Read database speficied table
 db = TinyDB('db_1.json')
+#Print default table
+db.default_table_name = 'table_2'
 #Create collection
-table1 = db.table('table_1')
+# table1 = db.table('table_1')
+# table2 = db.table('table_2')
 
 #Print all documents
-print(table1.all())
-
+# print(table1.all())
+print(db.all())
 
 
