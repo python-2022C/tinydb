1031cc18f6fbc7a4592b890965c0940ee7530543
ZARIFMAC <uzbzarif@gmail.com>
2022-09-20 10:41:57 +0500

    read by id

--- get_id.py
+++ get_id.py
@@ -2,7 +2,13 @@ from tinydb import TinyDB
 from tinydb.database import Document
 db = TinyDB('db.json')
 
+
 # Check if document exists
-if db.contains(doc_id=1):
-    print("Document exists")
+if db.contains(doc_id=5):
+    # Get document
+    doc = db.get(doc_id=1)
+    # Print document
+    print(doc)
+else:
+    print('Document does not exist')
     
