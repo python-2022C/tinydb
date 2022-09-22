a5503591167b6e4d600d4d8803208508ccedbac5
ZARIFMAC <uzbzarif@gmail.com>
2022-09-22 11:32:34 +0500

    indent

--- query_db.py
+++ query_db.py
@@ -2,12 +2,17 @@ from tinydb import TinyDB,Query
 from tinydb.database import Document
 
 #Read database speficied table
-db = TinyDB('data.json')
+db = TinyDB('data.json',indent=4,separators=(',',':'))
 #Collection or table
 db.default_table_name = 'Mobile'
 
 #Create query object
 query = Query()
 
-print(db.count(query.model.search('S10')))
+#Add new document
+db.insert({'name':'Samsung','price':10000,'model':'Galaxy S10'})
+db.insert({'name':'Samsung','price':20000,'model':'Galaxy S20'})
+db.insert({'name':'Samsung','price':30000,'model':'Galaxy S30'})
+
+
 
