7b9c002894656842379a1ea006db37ebd0fe1722
ZARIFMAC <uzbzarif@gmail.com>
2022-09-22 10:14:52 +0500

    get all documents

--- query_db.py
+++ query_db.py
@@ -3,7 +3,14 @@ from tinydb.database import Document
 
 #Read database speficied table
 db = TinyDB('data.json')
+#Collection or table
 db.default_table_name = 'Mobile'
+
+#Read all documents
+print(db.all())
+
+
+
 
 
 
