d3c3f9c697f4f50d2353098a7457404160e5a74d
ZARIFMAC <uzbzarif@gmail.com>
2022-09-22 10:54:17 +0500

    query task

--- query_db.py
+++ query_db.py
@@ -11,3 +11,15 @@ query = Query()
 
 
 print(db.search((query.price==500) | (query.price<=190))) #OR
+
+#Define query where price is less than 500
+#Define query where price is greater than 190
+#Define query where company is Samsung or Apple
+#Define query where company is Samsung and price is less than 500
+#Define query where company is Samsung and price is less than 500 and model is A10
+#Define query where company is Samsung and price is less than 500 and model is A10 or A20
+#Define query where companies are Samsung, Apple and Xiaomi
+#Define query where companies are Samsung, Apple and Xiaomi and price is less than 500
+#Define query where companies are Samsung, Apple and Xiaomi and price is less than 500 and model is A10
+#Define query where companies are Samsung, Apple and Xiaomi and price is less than 500 and model is A10 or A20
+#Define query where companies are Samsung, Apple and Xiaomi and color is black or white
